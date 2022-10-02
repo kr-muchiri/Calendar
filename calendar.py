649964b0191a3b3e7c278220e50d9f3a61f81651
Muchiri <79175191+kr-muchiri@users.noreply.github.com>
2022-10-01 23:19:42 -0400

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -3,4 +3,9 @@ from calendar import *
 year = int(input('Enter Year:'))
 print(calendar(year,3,1,8,4))
 
+#3 = 3 characters for the day of the week eg Mon, Tue
+#1 = 1 row for each week
+#8 = 8 rows for each month
+#4 = 4 columns for all months of the year
+
 
